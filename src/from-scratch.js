fc453d75330b6db71a132cf45a6297491b0506c7
Julioj1 <dbjulioj@gmail.com>
2025-10-15 15:31:51 -0400

    Completed listAllValues

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -46,7 +46,8 @@ const listAllKeys = (object) => {
   return (Object.keys(object));
 };
 
-const listAllValues = () => {
+const listAllValues = (object) => {
+  return (Object.values(object));
 };
 
 const convertToMatrix = () => {
